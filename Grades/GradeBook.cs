5a0b5d22db561b93651db78dfefacbf21cebe522
tmacak <tom.macak@gmail.com>
2016-05-18 12:17:23 -0700

    Classes and Objects Lesson

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,11 +8,24 @@ namespace Grades
 {
     class GradeBook
     {
-        List<float> grades;
-
         public GradeBook()
         {
             grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
         }
 
         public void AddGrade(float grade)
@@ -20,5 +33,6 @@ namespace Grades
             grades.Add(grade);
         }
         
+        private List<float> grades;
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -14,6 +14,12 @@ namespace Grades
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.AverageGrade);
         }
     }
 }
