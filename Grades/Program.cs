a22a7bfc7b6abaf870ef1275da60d4e3039011dc
tmacak <tom.macak@gmail.com>
2016-07-13 11:26:14 -0700

    Created Unit Tests

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -11,6 +11,7 @@ namespace Grades
 
         static void Main(string[] args)
         {
+
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
